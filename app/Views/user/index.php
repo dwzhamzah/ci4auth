41b5d8fbaf6065443d38ce8d74fd58e767ecd515
dwzhamzah <dwzhamzah@gmail.com>
2021-02-23 18:15:43 +0700

    Push all data for the first time

---
+++ app/Views/user/index.php
@@ -0,0 +1,10 @@
+<?= $this->extend('templates/index'); ?>
+
+<?= $this->section('admincontent'); ?>
+
+<div class="container-fluid">
+    <h1 class="h3 mb-4 text-grey-800">My Profile</h1>
+
+</div>
+
+<?= $this->endsection();  ?>
